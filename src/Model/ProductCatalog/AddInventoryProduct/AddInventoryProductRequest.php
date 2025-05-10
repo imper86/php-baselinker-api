0f0b05cb747e6136e8d361f1ce7061600c49c2aa
imper <me@imper.info>
2025-05-10 15:52:17 +0200

    added getters/setters on AddInventoryProductRequest

--- src/Model/ProductCatalog/AddInventoryProduct/AddInventoryProductRequest.php
+++ src/Model/ProductCatalog/AddInventoryProduct/AddInventoryProductRequest.php
@@ -104,4 +104,224 @@ class AddInventoryProductRequest implements RequestInterface
             'bundle_products' => $this->bundleProducts,
         ];
     }
+
+    public function getInventoryId(): string
+    {
+        return $this->inventoryId;
+    }
+
+    public function setInventoryId(string $inventoryId): void
+    {
+        $this->inventoryId = $inventoryId;
+    }
+
+    public function getProductId(): ?string
+    {
+        return $this->productId;
+    }
+
+    public function setProductId(?string $productId): void
+    {
+        $this->productId = $productId;
+    }
+
+    public function getParentId(): ?string
+    {
+        return $this->parentId;
+    }
+
+    public function setParentId(?string $parentId): void
+    {
+        $this->parentId = $parentId;
+    }
+
+    public function getIsBundle(): ?bool
+    {
+        return $this->isBundle;
+    }
+
+    public function setIsBundle(?bool $isBundle): void
+    {
+        $this->isBundle = $isBundle;
+    }
+
+    public function getEan(): ?string
+    {
+        return $this->ean;
+    }
+
+    public function setEan(?string $ean): void
+    {
+        $this->ean = $ean;
+    }
+
+    public function getSku(): ?string
+    {
+        return $this->sku;
+    }
+
+    public function setSku(?string $sku): void
+    {
+        $this->sku = $sku;
+    }
+
+    public function getTaxRate(): ?float
+    {
+        return $this->taxRate;
+    }
+
+    public function setTaxRate(?float $taxRate): void
+    {
+        $this->taxRate = $taxRate;
+    }
+
+    public function getWeight(): ?float
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(?float $weight): void
+    {
+        $this->weight = $weight;
+    }
+
+    public function getHeight(): ?float
+    {
+        return $this->height;
+    }
+
+    public function setHeight(?float $height): void
+    {
+        $this->height = $height;
+    }
+
+    public function getWidth(): ?float
+    {
+        return $this->width;
+    }
+
+    public function setWidth(?float $width): void
+    {
+        $this->width = $width;
+    }
+
+    public function getLength(): ?float
+    {
+        return $this->length;
+    }
+
+    public function setLength(?float $length): void
+    {
+        $this->length = $length;
+    }
+
+    public function getAverageCost(): ?float
+    {
+        return $this->averageCost;
+    }
+
+    public function setAverageCost(?float $averageCost): void
+    {
+        $this->averageCost = $averageCost;
+    }
+
+    public function getStar(): ?int
+    {
+        return $this->star;
+    }
+
+    public function setStar(?int $star): void
+    {
+        $this->star = $star;
+    }
+
+    public function getManufacturerId(): ?int
+    {
+        return $this->manufacturerId;
+    }
+
+    public function setManufacturerId(?int $manufacturerId): void
+    {
+        $this->manufacturerId = $manufacturerId;
+    }
+
+    public function getCategoryId(): ?int
+    {
+        return $this->categoryId;
+    }
+
+    public function setCategoryId(?int $categoryId): void
+    {
+        $this->categoryId = $categoryId;
+    }
+
+    public function getPrices(): ?array
+    {
+        return $this->prices;
+    }
+
+    public function setPrices(?array $prices): void
+    {
+        $this->prices = $prices;
+    }
+
+    public function getStock(): ?array
+    {
+        return $this->stock;
+    }
+
+    public function setStock(?array $stock): void
+    {
+        $this->stock = $stock;
+    }
+
+    public function getLocations(): ?array
+    {
+        return $this->locations;
+    }
+
+    public function setLocations(?array $locations): void
+    {
+        $this->locations = $locations;
+    }
+
+    public function getTextFields(): ?array
+    {
+        return $this->textFields;
+    }
+
+    public function setTextFields(?array $textFields): void
+    {
+        $this->textFields = $textFields;
+    }
+
+    public function getImages(): ?array
+    {
+        return $this->images;
+    }
+
+    public function setImages(?array $images): void
+    {
+        $this->images = $images;
+    }
+
+    public function getLinks(): ?array
+    {
+        return $this->links;
+    }
+
+    public function setLinks(?array $links): void
+    {
+        $this->links = $links;
+    }
+
+    public function getBundleProducts(): ?array
+    {
+        return $this->bundleProducts;
+    }
+
+    public function setBundleProducts(?array $bundleProducts): void
+    {
+        $this->bundleProducts = $bundleProducts;
+    }
 }
